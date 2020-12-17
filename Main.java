45b93b9862208fe292ebce71e0d0173848b1bea0
Taranjot Singh <61469964+dhillon7taran@users.noreply.github.com>
2020-12-17 12:14:58 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+ class Name {
+
+    public static void main(String[] args) {
+
+        // write your code here
+        System.out.println("Hello taranjot");
+        System.out.println("enter your name");
+        Scanner i = new Scanner(System.in);
+        String name = i.nextLine();
+        System.out.println("welcome to selenium"+" "+ name);
+        System.out.println("welcome to git");
+
+    }
+}
+
