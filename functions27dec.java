c4f82984989824fa8876da300d815edf4bc61413
Taranjot Singh <61469964+dhillon7taran@users.noreply.github.com>
2020-12-28 11:24:21 -0500

    Add files via upload

---
+++ functions27dec.java
@@ -0,0 +1,17 @@
+public class functions27dec {
+    private static int array;
+
+    public static void main(String[] args) {
+        int Array[]={1,2,3,4};
+        System.out.println(Sumofarray(Array));
+}
+
+    public static int Sumofarray(int Array[]) {
+        int sum=0;
+        for (int i=;i++;i<4)
+        {
+            sum=sum+Array[i];
+        }
+        return sum;
+    }
+}
